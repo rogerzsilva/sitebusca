744cd1a67c38a526d76955b316d673d6950c7fca
Rogerio <rogerz0610@gmail.com>
2020-05-04 19:14:13 -0300

    adicionando links no menu e corrigindo cards

--- js/busca.js
+++ js/busca.js
@@ -1,24 +1,36 @@
 //JSON
 let prestadores = [
     {
-        "nome":"felipe1",
-        "atuação":["home-care","fisioterapia","medico"],
-        "imagem":"grounded-coffee-shop-logo.jpg"
+        "nome":"Psicólogo",
+        "atuação":["home care","fisioterapia","medico"],
+        "descricao":"Psicólogos que fazem consultas na sua casa.",
+        "imagem":"psicologoshomecare.jpg",
+        "telefone":"(11) 99998-9109",
+        "site":"https://www.saidsp.com"
     },
     {
-        "nome":"felipe",
-        "atuação":["saude"],
-        "imagem":"grounded-coffee-shop-logo.jpg"
+        "nome":"Vacinas",
+        "atuação":["saude","vacina","medico"],
+        "descricao":"Médicos que vão até sua casa para aplicar medicamentos.",
+        "imagem":"vacinaemcasa.jpeg",
+        "telefone":"(11) 3042-1442",
+        "site":"https://beepsaude.com.br/"
     },
     {
-        "nome":"felipe",
-        "atuação":["transporte"],
-        "imagem":"grounded-coffee-shop-logo.jpg"
+        "nome":"Táxis Adaptados",
+        "atuação":["transporte","taxi"],
+        "descricao":"Táxis adaptados para pessoas com deficiência. Destina-se, exclusivamente, às pessoas com autismo, surdocegueira ou deficiência física e mobilidade altamente reduzida.",
+        "imagem":"unnamed.jpg",
+        "telefone":"(11)96865-9246",
+        "site":"http://www.sptrans.com.br/atende/"
     },
     {
-        "nome":"felipe2",
-        "atuação":["home-care"],
-        "imagem":"grounded-coffee-shop-logo.jpg"
+        "nome":"Nutricionista",
+        "atuação":["nutricionista","home care"],
+        "descricao":"Nutricionistas que fazem consulta em casa.",
+        "imagem":"nutricionistaemcasa.jpeg",
+        "telefone":"(11) 97518-8167",
+        "site":"http://www.homeangels.com.br/sp-vilamariana/"
     }
 ];
 //Valida se o que foi enviado, coincide com alguma atuação
@@ -48,14 +60,13 @@ function busca(area) {
 }
 //Monta HTML e coloca na pagina 
 function insereHTML(prestador){
-    var html = '<div class="col-lg-4 col-md-6 col-sm-8 col-10 resultado-prestador">';
-    html += '<div class="blog-post">';
-    html += '<div class="image" style="margin-left:60px;">';
-    html += '<img src="./images/'+prestador.imagem+'" width="200px" height="150px" alt="post-image" class="img-fluid">';
-    html += '</div>';
-    html += '<div class="post-content">';
-    html += '<div class="post-title">';
-    html += '<h5><a href="#">'+prestador.nome+'</a></h5>';
+    var html = '<div class="col-sm"><div class="card h-70 resultado-prestador" style="width: 18rem;">';
+    html += '<img class="card-img-top" src="img/'+prestador.imagem+'">';
+    html += '<div class="card-body">';
+    html += '<h5 class="card-title">'+prestador.nome+'</h5>';
+    html += '<p class="card-text">'+prestador.descricao+'</p>';
+    html += '<a href="'+prestador.telefone+'" class="card-link">Tel: '+prestador.telefone+'</a>';
+    html += '<a href="'+prestador.site+'" class="card-link">Visite nosso Site.</a>';
     html += '</div>';
     html += '</div>';
     html += '</div>';
